02cc5325d170e3cbc9252fc9e4e98fe1aadd8523
Ayoub Koudia <akoudia@c1r5p10.1337.ma>
2022-10-12 14:48:20 +0100

    666

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,8 +6,25 @@
 /*   By: akoudia <akoudia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 12:28:45 by akoudia           #+#    #+#             */
-/*   Updated: 2022/10/06 14:29:19 by akoudia          ###   ########.fr       */
+/*   Updated: 2022/10/12 13:10:46 by akoudia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*result;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	result = malloc(len1 + len2 + 1);
+	if (result)
+	{
+		ft_memcpy(result, s1, len1);
+		ft_memcpy(result + len1, s2, len2 + 1);
+	}
+	return (result);
+}
