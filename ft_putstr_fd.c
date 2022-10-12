50de9df81021197d9390278e80e89f149c74a48d
Ayoub Koudia <akoudia@c1r5p10.1337.ma>
2022-10-12 14:49:13 +0100

    666

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,12 +6,11 @@
 /*   By: akoudia <akoudia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 12:28:36 by akoudia           #+#    #+#             */
-/*   Updated: 2022/10/09 09:31:10 by akoudia          ###   ########.fr       */
+/*   Updated: 2022/10/12 14:49:03 by akoudia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
-#include "libft.h"
 
 void	ft_putstr_fd(char *s, int fd)
 {
