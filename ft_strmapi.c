bf5d4f83ccaab41371bc17c384340ed9307cc866
Ayoub Koudia <akoudia@c1r5p10.1337.ma>
2022-10-12 16:01:07 +0100

    666

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,8 +6,29 @@
 /*   By: akoudia <akoudia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 12:28:48 by akoudia           #+#    #+#             */
-/*   Updated: 2022/10/06 14:29:23 by akoudia          ###   ########.fr       */
+/*   Updated: 2022/10/12 16:00:43 by akoudia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int		i;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
