f0062d9ca2415b6ad13cc74b1dc25a0addce70c4
ayoub koudia <93022275+ayoubkoudia@users.noreply.github.com>
2022-10-18 10:13:56 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -11,62 +11,97 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-int	ft_wordcompt(char *str, char sep)
+int	ft_countdest(char const *s, char c)
 {
 	int	i;
-	int	count;
-
-	count = 0;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] != sep)
-		{
-			while (str[i] != sep && str[i])
-				i++;
-			count++;
-		}
-		else
-			i++;
-	}
-	return (count);
-}
-
-char	**ft_sep(char *str, char **dest, char sep, int count)
-{
-	int	k;
 	int	j;
-	int	i;
 
 	i = 0;
 	j = 0;
-	while (str[i] != '\0' && j < count)
+	if (!s[i])
+		return (0);
+	while (s[i] != '\0')
 	{
-		k = 0;
-		while (str[i] == sep)
+		while (s[i] != c && s[i] != '\0')
 			i++;
-		while (str[i + k] != sep && str[i + k] != 0)
-			k++;
-		dest[j] = ft_substr(str, i, k);
-		if (! dest[j++])
-			return (NULL);
-		i = i + k;
+		if (s[i] != '\0' && s[i] == c)
+		{
+			while (s[i] == c)
+				i++;
+			j++;
+		}
 	}
-	dest[j] = NULL;
+	if (s[0] != c && s[0] != '\0')
+		j++;
+	if (s[i - 1] == c)
+		j--;
+	return (j);
+}
+
+static char	*ft_filldest(char const *s, int i, int j)
+{
+	char	*dest;
+	int		n;
+
+	n = 0;
+	dest = malloc(sizeof(char) * (i + 1));
+	if (!dest)
+		return (0);
+	j = j - i;
+	while (i > 0)
+	{
+		dest[n++] = s[j++];
+		i--;
+	}
+	dest[n] = '\0';
 	return (dest);
 }
 
-char	**ft_split(char const *str, char sep)
+char	**ft_split(char const *s, char c)
 {
 	char	**dest;
-	int		count;
+	int		j;
+	int		i;
+	int		n;
 
-	if (!str)
-		return (NULL);
-	count = ft_wordcompt((char *)str, sep);
-	dest = (char **)malloc((count + 1) * sizeof(char *));
-	if (!dest)
-		return (NULL);
-	return (ft_sep((char *)str, dest, sep, count));
-}
+	i = 0;
+	j = 0;
+	dest = malloc(sizeof(char *) * (ft_countdest(s, c) + 1));
+	if (!dest || !s)
+		return (0);
+	while (s[i] != '\0')
+	{
+		n = i;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		if (i != 0)
+		{
+			dest[j] = ft_filldest(s, i - n, i);
+			j++;
+		}
+		while (s[i] == c && s[i] != '\0')
+			i++;
+	}
+	dest[j] = 0;
+	return (dest);
+}	
+
+/*int	main()
+{
+	char str[] = "";
+	char c = 'z';
+	printf("%s", ft_split(str, c)[0]);
+	printf("\n");
+	printf("%s", ft_split(str, c)[1]);
+        printf("\n");
+	printf("%s", ft_split(str, c)[2]);
+        printf("\n");
+	printf("%s", ft_split(str, c)[3]);
+        printf("\n");
+	printf("%s", ft_split(str, c)[4]);
+        printf("\n");
+	printf("%s", ft_split(str, c)[5]);
+        printf("\n");
+}*/
