6138e33d8cd380cfc3a4ed52453a1172f163f24c
Ayoub Koudia <akoudia@c1r8p4.1337.ma>
2022-10-19 17:02:50 +0100

    69

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,28 +6,8 @@
 /*   By: akoudia <akoudia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 12:28:53 by yelaissa          #+#    #+#             */
-/*   Updated: 2022/10/19 15:47:09 by akoudia          ###   ########.fr       */
+/*   Updated: 2022/10/19 15:52:59 by akoudia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list	*newlst;
-	t_list	*newnode;
-
-	newlst = NULL;
-	while (lst)
-	{
-		newnode = ft_lstnew(f(lst->content));
-		if (newnode == 0)
-		{
-			ft_lstclear(&newlst, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&newlst, newnode);
-		lst = lst->next;
-	}
-	return (newlst);
-}
